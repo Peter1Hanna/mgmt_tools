68551727ed1c7d1317c4fa312e8e34f161a35ec4
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-06-21 21:05:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,18 @@
 #DDC PRTG VMs
 module "prtg_ddc" {
   source      = "./modules/prtg_ddc"
+    user           = "al.jain@mgmt.certapay.com"
+  password       = "VMw@reNew!!"
 }
 
 module "prtg_mdc" {
   source      = "./modules/prtg_mdc"
-
+  user           = "al.jain@mgmt.certapay.com"
+  password       = "VMw@reNew!!"
 }
 
 module "prtg_qa" {
  source      = "./modules/prtg_qa"
-
+  user           = "al.jain@mgmt.certapay.com"
+  password       = "VMw@reNew!!"
 }
