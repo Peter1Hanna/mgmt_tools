cc376b1b1b36d2eda393df08b6ec4e032f169239
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-06-21 20:48:52 -0400

    Update prtg_mdc.tf

--- modules/prtg_qa/prtg_mdc.tf
+++ modules/prtg_qa/prtg_mdc.tf
@@ -41,8 +41,8 @@ data "vsphere_resource_pool" "pool" {
 }
 
 provider "vsphere" {
-  user           = var.vcenter_username
-  password       = var.vcenter_username
+  user           = "al.jain@mgmt.certapay.com"
+  password       = "VMw@reNew!!"
   vsphere_server = "10.6.15.37"
   #version = "~&gt; 1.11"
  
