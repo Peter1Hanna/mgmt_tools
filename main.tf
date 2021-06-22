82d9ea1e71c354cdda1b1521c087eefb28ca3059
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-06-21 20:34:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ vcenter_password = "${var.vcenter_password}"
 */
 module "prtg_qa" {
  source      = "./modules/prtg_mdc"
- vcenter_username = "${var.vcenter_username}"
- vcenter_password = "${var.vcenter_password}"
+ user = "${var.vcenter_username}"
+ password = "${var.vcenter_password}"
 }
