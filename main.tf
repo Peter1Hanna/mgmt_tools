3762e88e17593cd2f6a69604e096a6db10fe78a8
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-06-21 21:00:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,15 @@
-variable "vcenter_username" {
-  description = "vCenter administrator username"
-  type        = string
-}
 
-variable "vcenter_password" {
-  description = "vCenter administrator password"
-  type        = string
-}
 #DDC PRTG VMs
-
 module "prtg_ddc" {
   source      = "./modules/prtg_ddc"
-
-vcenter_username = "al.jain@mgmt.certapay.com"
-vcenter_password = "VMw@reNew!!"
 }
 
 module "prtg_mdc" {
   source      = "./modules/prtg_mdc"
-vcenter_username = "al.jain@mgmt.certapay.com"
-vcenter_password = "VMw@reNew!!"
+
 }
 
 module "prtg_qa" {
  source      = "./modules/prtg_qa"
-vcenter_username = "al.jain@mgmt.certapay.com"
-vcenter_password = "VMw@reNew!!"
+
 }
