95408496a427f7c3c73b23a780c2a98b7b0ff416
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-06-21 23:04:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,22 +8,8 @@ variable "vcenter_password" {
   type        = string
 }
 
-module "prtg_ddc" {
-  source      = "./modules/prtg_ddc"
-
-vcenter_username = var.vcenter_username
-vcenter_password = var.vcenter_password
-}
-
-module "prtg_mdc" {
-  source      = "./modules/prtg_mdc"
-
-vcenter_username = var.vcenter_username
-vcenter_password = var.vcenter_password
-}
 module "prtg_qa" {
   source      = "./modules/prtg_qa"
-
 vcenter_username = var.vcenter_username
 vcenter_password = var.vcenter_password
 }
